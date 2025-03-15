773f68fb8d986eef21bfe1966bedf26e35958fae
ISHANT KUMAR YADAV <114832189+yishant11@users.noreply.github.com>
2025-03-15 23:52:53 +0530

    second commit

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://job-portal-frontend-teal.vercel.app",
     credentials: true,
   })
 );
